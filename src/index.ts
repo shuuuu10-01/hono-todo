566b6f60f1ce55b18f841d0481db8d6efd598d20
shuuuu10-01 <koron.konan123@gmail.com>
2024-06-07 08:15:40 +0900

    feat: corsエラーを回避できるように設定追加

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { Hono } from "hono";
 import { z } from "zod";
 import { zValidator } from "@hono/zod-validator";
+import { cors } from "hono/cors";
 
 type Bindings = {
   DB: D1Database;
@@ -12,6 +13,8 @@ type Todo = {
 };
 
 const app = new Hono<{ Bindings: Bindings }>();
+
+app.use("*", cors());
 
 app.get("/", async (c) => {
   const { results } = await c.env.DB.prepare(
