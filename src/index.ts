e261fbe4a36063bc0d8a59d725f27efc31f987af
shuuuu10-01 <koron.konan123@gmail.com>
2024-06-06 23:06:13 +0900

    feat: todoの各エンドポイント作成

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,49 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
+import { z } from "zod";
+import { zValidator } from "@hono/zod-validator";
 
-const app = new Hono()
+type Bindings = {
+  DB: D1Database;
+};
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+type Todo = {
+  title: string;
+  id: string;
+};
 
-export default app
+const app = new Hono<{ Bindings: Bindings }>();
+
+app.get("/", async (c) => {
+  const { results } = await c.env.DB.prepare(
+    `SELECT id, title FROM todo;`
+  ).all<Todo>();
+  return c.json(results);
+});
+
+app.post(
+  "/todo",
+  zValidator(
+    "json",
+    z.object({
+      title: z.string().min(1),
+    })
+  ),
+  async (c) => {
+    const { title } = c.req.valid("json");
+    const id = crypto.randomUUID();
+    await c.env.DB.prepare(`INSERT INTO todo(id, title) VALUES(?, ?);`)
+      .bind(id, title)
+      .run();
+    c.status(200);
+    return c.body(null);
+  }
+);
+
+app.delete("/todo/:id", async (c) => {
+  const id = c.req.param("id");
+  await c.env.DB.prepare(`DELETE FROM todo WHERE id = ?;`).bind(id).run();
+  c.status(200);
+  return c.body(null);
+});
+
+export default app;
